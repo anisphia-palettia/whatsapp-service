e2fbb4584ad1294e40d73fa081826fd598532fdf
anisphia-palettia <euphyllia.tomodachi@gmail.com>
2025-07-17 10:08:36 +0700

    loger update

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -7,23 +7,26 @@ import dayjs from "dayjs";
 const pathLog = path.join(process.cwd(), "logs", "app.log")
 fs.mkdirSync(path.dirname(pathLog), {recursive: true})
 
-export const logger = appConfig.nodeEnv === "production" ? pino({
-    transport: {
-        target: "pino/file",
-        options: {
-            destination: pathLog,
-            mkdir: true,
+export const logger = appConfig.nodeEnv === "production"
+    ? pino({
+        transport: {
+            target: "pino/file",
+            options: {
+                destination: pathLog,
+                mkdir: true,
+            },
         },
-    },
-    timestamp: () => `,"time":"${dayjs().format()}"`,
-}) : pino({
-    level: 'debug', transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            translateTime: "HH:MM:ss",
-            ignore: "pid,hostname",
+        timestamp: () => `,"time":"${dayjs().format('DD-MM-YYYY HH:mm:ss')}"`,
+    })
+    : pino({
+        level: 'debug',
+        transport: {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "HH:MM:ss",
+                ignore: "pid,hostname",
+            },
         },
-    },
-    timestamp: () => `,"time":"${dayjs().format()}"`,
-});
+        timestamp: () => `,"time":"${dayjs().format('DD-MM-YYYY HH:mm:ss')}"`,
+    });
